bfc78309197221cd19f5fabbb996b7b47b73570d
ShamilShigapov <sshigapov1990@gmail.com>
2025-11-04 14:37:46 +0300

    удалил ненужные view

--- FitnessCentrApp/Views/ValidationRules/PhoneValidationRule.cs
+++ FitnessCentrApp/Views/ValidationRules/PhoneValidationRule.cs
@@ -12,7 +12,7 @@ public class PhoneValidationRule : ValidationRule
         if (string.IsNullOrWhiteSpace(phone))
             return new ValidationResult(false, "Введите номер телефона");
 
-        if (!Regex.IsMatch(phone, @"^\+7\s?\(?\d{3}\)?\s?\d{3}-?\d{2}-?\d{2}$"))
+        if (!Regex.IsMatch(phone, @"^(\+7|8)\s?\(?\d{3}\)?\s?\d{3}-?\d{2}-?\d{2}$"))
             return new ValidationResult(false, "Неверный формат номера");
 
         return ValidationResult.ValidResult;
